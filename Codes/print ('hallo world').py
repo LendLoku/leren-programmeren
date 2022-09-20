c3d69f4da69084d74f99dac4ad35166ceeb7496a
Lend Loku <99072333@mydavinci.nl>
2022-09-20 09:51:04 +0200

    initial

---
+++ Codes/print ('hallo world').py
@@ -0,0 +1,18 @@
+
+naam = input ('hoe heet je?: ')
+
+leeftijd = input ('hoe oud ben je?: ')
+
+vraag1 = input ('bent u een man?: ')
+vraag1.lower()
+
+if vraag1 in ('nee'):
+    lichaamvraag2 = (input('hoelang is uw haar?: ')(print ('zo dat is flink wat haar')))
+
+if vraag1 in ('ja'): 
+    lichaamvraag1 = (input('heeft u een baard?: '))
+if lichaamvraag1 in ('ja'):
+    baardvraag1 = (input('hoelang is uw baard?:'))
+
+print ('dat is een flinke baard')
+
