9af32539efa26ea8c3ddd04651652a861b05ee27
Lend Loku <99072333@mydavinci.nl>
2022-09-21 13:27:44 +0200

    mappen

---
+++ leren-programmeren/module 1/deel 1/# Ingredienten voor Tzatziki.py
@@ -0,0 +1,22 @@
+# Ingredienten voor Tzatziki
+        
+print ('Berekenen van de ingredienten')
+aantalKomkommers = input('voor hoeveel komkommers? ')
+aantalKomkommers = int(aantalKomkommers)
+
+mespuntjesperkomkommer = 1 
+zeezoutpuntjes = aantalKomkommers * mespuntjesperkomkommer
+
+zeezout = aantalKomkommers * 1
+knoflook = aantalKomkommers * 2 
+VerseDille = aantalKomkommers * 15
+GriekseYogurt = aantalKomkommers * 500
+WitteWijnAzijn = aantalKomkommers * 1
+olijfolie = aantalKomkommers * 3
+
+print ( zeezoutpuntjes ,'mespuntjes zeezout')
+print ( knoflook ,'tenen knoflook')
+print ( VerseDille ,'gram verse dille')
+print ( GriekseYogurt ,' ml griekse yogurt')
+print ( WitteWijnAzijn ,'el witte wijn')
+print ( olijfolie  ,'el olijfolie')
---
+++ leren-programmeren/module 1/deel 1/demo vloer.py
@@ -0,0 +1,10 @@
+lengte = input('lengte: ')
+lengte = float(lengte)
+breedte = input('breedte:')
+breedte = float(breedte)
+oppvl = lengte * breedte
+
+omtrek =  2 * (lengte + breedte) 
+
+print (' Oppervlakte wordt: ', lengte * breedte,'m') # Deze instructie print de tekts
+print ('Omtrek wordt: ', omtrek,'m') 
