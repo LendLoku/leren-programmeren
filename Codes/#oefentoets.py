3ab0c87d0ed28d4de555d854a053253d1d6cdbf4
Lend Loku <99072333@mydavinci.nl>
2022-09-22 13:40:43 +0200

    oefentoets

--- Codes/#oefentoets.py
+++ Codes/#oefentoets.py
@@ -1,26 +1,34 @@
 #oefentoets
+iphone = int (input ('hoe duur is de Iphone 13 in euros? : '))
+samsung = int (input ('hoe duur is de Samsung Galaxy 22 in euros? : '))   
 
-iphone = input ('hoe duur is de Iphone 13 in euros? : ')
-iphone = int (iphone)
+max1 = iphone - samsung
+max2 = samsung - iphone 
 
-samsung = input ('hoe duur is de Samsung Galaxy 22 in euros? : ')
-samsung = int (samsung)
 
-evenduur = samsung == iphone
-iphoneduurder = iphone > samsung 
-samsungduurder = samsung > iphone 
+if (iphone > 900 and samsung > 900 ):
+    print ('Het advies is dus  geen telefoon te kopen, ze zijn te duur.')
+    exit()
 
-if iphoneduurder:
-    input (f'''de iphone is duurder dan de samsung 
+if max1 > 50:
+    print (f'''de iphone is duurder dan de samsung
 de iphone kost €{iphone}
 de goedkoopste is de samsung die kost €{samsung}
-het advies is dus om de samsung te kopen. Deze is namelijk {iphone - samsung} euro goedkoper dan de iphone''')
+het advies is dus om de samsung te kopen. Deze is namelijk {max1} euro goedkoper dan de iphone''')
 
-if samsungduurder:
-    input (f'''de samsung is duurder dan de iphone
+elif max2 < 50:
+    print (f'''de iphone is duurder dan de samsung
+de iphone kost €{iphone}
+de goedkoopste is de samsung die kost €{samsung}
+het advies is dus om de iphone te kopen. Deze is namelijk {max1} euro goedkoper dan de iphone''')
+
+elif samsung > iphone: 
+    print (f'''de samsung is duurder dan de iphone    
 de samsung is het duurst de telefoon kost €{samsung}
 de goedkoopste is de iphone die kost €{iphone}
-het advies is dus om de iphone te kopen. Deze is namelijk {samsung - iphone} euro goedkoper dan de samsung''')
+het advies is dus om de iphone te kopen. Deze is namelijk {max2} euro goedkoper dan de samsung''')
 
-elif evenduur: 
-    input ('de telefoons zijn even duur dus het maakt niet uit welke u wilt kiezen')   
+
+elif samsung == iphone:
+     input ('de telefoons zijn even duur dus het maakt niet uit welke u wilt kiezen')   
+
