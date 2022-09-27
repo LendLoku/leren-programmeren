14cec2a6bd0524cb1109edb09f8081ba1b1b497f
Lend Loku <99072333@mydavinci.nl>
2022-09-27 20:45:01 +0200

    opdrachten

---
+++ leren-programmeren/module 2/deel 3/pizzacalculator.py
@@ -0,0 +1,23 @@
+grootte = input('Kies de grootte: small, medium, large ')
+try:
+    aantal = int(input("Hoeveel pizza's wilt u? "))
+except:
+    print("Nee dat is niet de juiste eenheid. Voer een nummer in!")
+ 
+if grootte in("s","small","Small","S"):
+    prijs = 6.99
+if grootte in("m","medium","Medium","M"):
+    prijs = 8.99
+if grootte in("l","large","Large","L"):
+    prijs = 10.99
+
+totaalprijs = aantal * prijs
+
+print(f"""
+    BON
+    -------------------------------------
+    {aantal} {grootte} pizza's       {aantal}x {prijs},- €
+    
+    -------------------------------------
+    Totaal prijs:           {totaalprijs},- €
+    """)
