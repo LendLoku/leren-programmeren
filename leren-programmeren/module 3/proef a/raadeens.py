88aa9c6e95ba6832aad377ede71e202667c5d4bd
Lend Loku <99072333@mydavinci.nl>
2022-12-01 11:43:40 +0100

    raad

--- leren-programmeren/module 3/proef a/raadeens.py
+++ leren-programmeren/module 3/proef a/raadeens.py
@@ -1,4 +1,8 @@
 import random
+
+aantal_pogingen = 10
+score = 0
+poging = 0
 while True:
     try:
         rondes = int( input("Voer in hoeveel rondes u wilt spelen (maximaal 20): "))
@@ -7,56 +11,49 @@ while True:
         else:
             print("Boven de 0 en onder de 20!")
     except:
-        print("Geldig nummer invoeren!")
-
-score = 0
-teller = 1
-print("Gok een getal tussen de 0 en 1000!")
-while teller <= rondes:
-    print(f"Ronde {teller}")
-    rndnummer = random.randint(0, 1000)
+        print("voer een geldig nummer in!")
+print ('gok een getal tussen de 1 en 1000!')
+while poging < aantal_pogingen:
+    print(f"Ronde {poging}")
+    geheimgetal = random.randint (0, 1000)
     aantal_gokken = 1
-    while aantal_gokken <= 10:
+    while poging < aantal_pogingen:
         while True:
-            try:    
-                gok = int( input(f"Voer hier je gok in (gok {aantal_gokken}): "))
-                break
+            try:
+                gok = int(input(f'Voer hier je gok in (gok {aantal_gokken}): '))
             except:
-                print("Geldig nummer invoeren")
-        
-        if gok == rndnummer:
-            print("Goed geraden! Score + 1")
-            score += 1
-            aantal_gokken += 10
-            break
+                print ('Voer een geldig nummer in!')
 
-        if rndnummer > gok:
-            afstand = rndnummer - gok
-            print("Hoger")
-        elif rndnummer < gok:
-            afstand = gok - rndnummer
-            print("Lager")
-
-        if afstand < 50 and afstand > 20:
-            print("Je bent warm") 
-        elif afstand < 20:
-            print("Je bent heel warm")
-        aantal_gokken += 1
-    print(f"Het getal was: {rndnummer}")
-    print(f"Dit is uw score: {score}")
-    if rondes != teller:
-        while True:
-            nog_een_keer = input("Wilt u nog een keer: ").lower()
-            if nog_een_keer in ["ja", "nee"]:
+            if gok == geheimgetal:
+                print("goed geraden! Score + 1")
+                score += 1 
+                aantal_gokken += 10
                 break
-            else:
-                print("Geldig antwoord invoeren [Ja, Nee]")
-            
-        if nog_een_keer == "nee":
-            print(f"Uw eindscore was: {score}")
-            print("Bedankt voor het spelen!")
-            quit()
-    teller += 1
+                
+            if geheimgetal > gok:
+                afstand = geheimgetal - gok
+                print ('Het getal is hoger')
+            elif geheimgetal < gok:
+                afstand = gok - geheimgetal
+                print ('Het getal is lager')
 
-print(f"Uw eindscore was: {score}")
-print("Bedankt voor het spelen!")
+            if afstand < 50 and afstand > 20:
+                print ('Je bent warm!')
+            elif afstand < 20:
+                print ('Je bent heel erg warm')
+            aantal_gokken += 1
+        print (f'Het getal was: {geheimgetal}')
+        print (f'Dit was jou score: {score}')
+        if rondes != poging:
+            while True:
+                nog_een_keer = input('Wilt u het spel nog een keer spelen? (ja of nee): ').lower()
+                if nog_een_keer in ('ja', 'nee'):
+                    break
+            
+            if nog_een_keer == 'nee':
+                print (f'Uw eindscore is: {score}')
+                print ('Dankuwel voor het spelen van dit spel')
+                quit()
+            poging += 1
+print(f"Uw eindscore is: {score}")
+print("Dankuwel voor het spelen van dit spel")  
