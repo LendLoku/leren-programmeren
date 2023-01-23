fece8e870b1186af45582db32374d1cae91f45d3
Lend Loku <99072333@mydavinci.nl>
2023-01-23 13:24:53 +0100

    function

---
+++ leren-programmeren/module 5/deel 1a/hallo.py
@@ -0,0 +1,5 @@
+def welcome(function):
+    for x in range(function):
+        print(f'Hello from functiontown {x + 1}!')
+
+welcome(7)
---
+++ leren-programmeren/module 5/deel 1a/multiplier-x.py
@@ -0,0 +1,6 @@
+def multiplier(tafel):
+  for nummer in range(1, 11):
+    print(nummer, "x", tafel, "=", tafel*nummer)
+
+num = input("Van welk getal wilt u de tafel zien? ")
+multiplier(int(num))
