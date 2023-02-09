2c01ef5138f586a76e959a001280d98eaae66773
Lend Loku <99072333@mydavinci.nl>
2023-02-09 13:28:38 +0100

    function

--- leren-programmeren/module 5/deel 1a/functions.py
+++ leren-programmeren/module 5/deel 1a/functions.py
@@ -28,10 +28,7 @@ operations = {
 
 def get_number(prompt):
     while True:
-        try:
             return float(input(prompt))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
 
 
 result = None
