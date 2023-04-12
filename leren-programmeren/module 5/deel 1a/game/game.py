b41f62cd0723e2702ca4ffe69d0bfbfa81e934e8
Lend Loku <99072333@mydavinci.nl>
2023-04-12 11:41:27 +0200

    commits

--- leren-programmeren/module 5/deel 1a/game/game.py
+++ leren-programmeren/module 5/deel 1a/game/game.py
@@ -1,17 +1,15 @@
-
+from gamefunction import *
 print ('Welkom soldaat')
 print ('We hebben jou nodig om de baas van het andere leger uit te schakelen!\n')
-naam_soldaat = input ('Wat is uw naam soldaat?\n')
-print (f'{naam_soldaat} je gaat nu de bunker in van het andere leger.')
+naam_soldaat = naam()
+print ('je gaat nu de bunker in van het andere leger.')
 while True:
     richting1 = input ('welke kant kies je om verder te gaan aan de linker kant staan 3 soldaten te bewaken en aan de rechterkant staat 1 soldaat (links/rechts)\n')
     if richting1 in ['links','rechts']:
         break
 
-if richting1 == 'links':
-    print ('U bent helaas gesneuveld')
-    print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.') 
-    exit ()
+if richting1 == dead():
+    exit()
 elif richting1 == 'rechts':
     print ('U hebt de soldaat dood gemaakt hij laat ze wapen vallen')
 
@@ -20,10 +18,10 @@ elif richting1 == 'rechts':
         if wapen in ['ja','nee']:
             break
     if wapen == 'ja':
-        print (f'Goed gedaan {naam_soldaat} nu heb je een wapen om je zelf te beschermen!')
+        print (f'Goed gedaan nu heb je een wapen om je zelf te beschermen!')
 
     else:
-        print ('Jij bent echt dom zo win je nooit')
+        dead()
         exit ()
 
 print ('Je komt nu op een kruising waar je naar links kan, recht en rechtdoor')
@@ -31,15 +29,10 @@ while True:
     richting2 = input ('Je komt aan de linker kant 2 soldaten tegen, rechts kom je iets tegen wat je nooit hebt gezien en rechtdoor staat er iets op je te wachten (links/rechts/rechtdoor) \n')
     if richting2 in ['links','rechts', 'rechtdoor']:
         break
-if richting2 == 'links':
-    print ('U bent helaas gesneuveld')
-    print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven. \n') 
-    exit ()
+if richting2 == left():
+     exit()
 
-if richting2 == 'rechtdoor':
-        print ('Er stonden 200 soldaten klaar om het gevecht dat buiten bezig is in te gaan')
-        print ('U bent helaas gesneuveld')
-        print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.') 
+if richting2 == rechtdoor():
         exit ()
 if richting2 == 'rechts':
             print ('Je ziet een kamer met een raam en door die raam zie je camerabeelden')
@@ -48,20 +41,15 @@ if richting2 == 'rechts':
 
 kamer1 = input ('Welke kamer wil je als eerste bekijken, in de linker kamer staan 10 soldaten te bewaken en rechts kan je niet goed zien want het is heel donker. (links/rechts)\n')
 
-if kamer1 == 'links':
-         print ('Die 10 soldaten hebben je vol door je hoofd heen geschoten ze hoorde je voet stappen.')
-         print ('U bent helaas gesneuveld')
-         print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.') 
-         exit ()
+if kamer1 == left():
+     exit()
 
 if kamer1 == 'rechts':
         print ('Je bent nu in het rechter kamer') 
         print ('Hier staat een soldaat, deze soldaat ligt te slapen dus je moet zo stil mogelijk naar binnen lopen')
 kill1 = input ('Je loopt er heel stil naar toe, wil je hem in zijn nek steken of met je ak47 schieten. (mes/ak47)\n')
 
-if kill1 == 'ak47':
-    print ('Dit was een domme zet je schiet dwars door zijn hoofd maar dat schot was zo luid dat er gelijk 100 soldaten naar je toe kwamen')
-    print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.\n') 
+if kill1 == ak47():
     exit ()
 
 elif kill1 == 'mes':
@@ -81,22 +69,14 @@ while True:
         terug = input ('Wil je terug rennen (ja/nee)')
         if terug == 'nee':
             print ('Maar een van de 300 soldaten zag jou en rent nu achter jou aan met 60 man en schieten jou dood')
-            print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.\n') 
+        dead()
         exit ()
     break
 richting4 = input ('Kies je rechtdoor of rechts')
 
 if richting3 == 'rechtdoor' or richting4 == 'rechtdoor':
-    print ('Je loopt rechtdoor het word helemaal donker in de lange gang waar je nu in loopt en je hebt geen zaklamp of iets dat licht geeft')
-    print ('Je ziet licht aan het einde en rent er snel naar toe')
-    print ('Je bent er eindelijk en is het een van de 400 leger autos die aan staan waar 400 soldaten in staan')
-    print ('Je word nu vol door je hoofd heen geschoten door de bewakers die daar staan')
-    print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.\n') 
+    VoorHetEinde()
     exit ()
 elif richting3 == 'rechts' or richting4 == 'rechtdoor':
-    print ('Je schiet de 4 soldaten neer en je werd in je been geraakt.')
-    print ('Je loopt nu mank maar je komt de leider tegen die je dood wou hebben')
-    print ('Je hebt de leider dood geschoten waardoor de tegenstanders zich moeten overgeven aan jou land.')
-    print ('Je bloed nu langzaam dood maar hebt wel de leider dood')
-    print (f'{naam_soldaat} u hebt gevochten als een echte soldaat, je zal altijd in ons hart blijven.') 
+    einde()
     exit ()
---
+++ leren-programmeren/module 5/deel 1a/game/gamefunction.py
@@ -0,0 +1,31 @@
+def naam():
+    input('wat is uw naam soldaat: ')
+
+def dead():
+    print('Goed gevochten strijder.')
+
+def right():
+    dead()
+
+def left():
+    dead()
+
+def rechtdoor():
+    dead()
+
+def ak47():
+    dead()
+
+def einde():
+    print ('Je schiet de 4 soldaten neer en je werd in je been geraakt.')
+    print ('Je loopt nu mank maar je komt de leider tegen die je dood wou hebben')
+    print ('Je hebt d4e leider dood geschoten waardoor de tegenstanders zich moeten overgeven aan jou land.')
+    print ('Je bloed nu langzaam dood maar hebt wel de leider dood')
+    dead()
+
+def VoorHetEinde():
+    print ('Je loopt rechtdoor het word helemaal donker in de lange gang waar je nu in loopt en je hebt geen zaklamp of iets dat licht geeft')
+    print ('Je ziet licht aan het einde en rent er snel naar toe')
+    print ('Je bent er eindelijk en is het een van de 400 leger autos die aan staan waar 400 soldaten in staan')
+    print ('Je word nu vol door je hoofd heen geschoten door de bewakers die daar staan')
+    dead()
